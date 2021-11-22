1ec0ded641eca30fd9f12af9b9e4827cf838f695
Miguel <al341802@uji.es>
2021-11-22 16:25:07 +0100

    Instructions for basic rescheduling

--- app/src/main/java/es/uji/geotec/ipin/alarm/ScanAlarmReceiver.java
+++ app/src/main/java/es/uji/geotec/ipin/alarm/ScanAlarmReceiver.java
@@ -10,6 +10,12 @@ import es.uji.geotec.ipin.scan.ScanManager;
 public class ScanAlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        // TODO: reschedule the alarm
+        // Steps:
+        //      - Create a new ScanAlarmManager instance using context.
+        //          - Schedule alarm --> ScanAlarmManager#schedule(interval).
+        //          - Interval --> ScanAlarmManager.INTERVAL (one minute).
+
         ScanManager scanManager = new ScanManager(context, "a9c04048-a71e-42b5-b569-13b5ac77b618");
         scanManager.startScanAndStoreFingerprints(ScanManager.SCANNING_TIME)
                 .subscribe(() -> Log.d("ScanAlarmReceiver", "scan ended!"));
