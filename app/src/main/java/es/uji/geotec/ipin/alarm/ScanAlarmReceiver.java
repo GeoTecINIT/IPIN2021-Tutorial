b1bd3e99742d1a8fbfdcd711f9ad15b8cc202db2
Miguel <al341802@uji.es>
2021-11-23 09:09:47 +0100

    Implement background service

--- app/src/main/java/es/uji/geotec/ipin/alarm/ScanAlarmReceiver.java
+++ app/src/main/java/es/uji/geotec/ipin/alarm/ScanAlarmReceiver.java
@@ -3,9 +3,8 @@ package es.uji.geotec.ipin.alarm;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
-import es.uji.geotec.ipin.scan.ScanManager;
+import es.uji.geotec.ipin.service.ScanService;
 
 public class ScanAlarmReceiver extends BroadcastReceiver {
     @Override
@@ -13,12 +12,7 @@ public class ScanAlarmReceiver extends BroadcastReceiver {
         ScanAlarmManager scanAlarmManager = new ScanAlarmManager(context);
         scanAlarmManager.schedule(ScanAlarmManager.INTERVAL);
 
-        // TODO: move the scanning work to ScanService
-        // Steps:
-        //      - Create a new Intent using the context and a reference to the ScanService class
-        //      - Start the service by calling context.startService(intent)
-        ScanManager scanManager = new ScanManager(context, "a9c04048-a71e-42b5-b569-13b5ac77b618");
-        scanManager.startScanAndStoreFingerprints(ScanManager.SCANNING_TIME)
-                .subscribe(() -> Log.d("ScanAlarmReceiver", "scan ended!"));
+        Intent serviceIntent = new Intent(context, ScanService.class);
+        context.startService(serviceIntent);
     }
 }
